e2d467613322a1f7049ef8df7519fbaca37dda13
Kaynat Zehra <kaynatzehra23@gmail.com>
2022-04-10 14:15:00 +0530

    Added: Address Book App Program

---
+++ js/utility.js
@@ -0,0 +1,25 @@
+const strigifyDate = (date) => {
+    const options = {day:'numeric', month:'short', year:'numeric'};
+    const newDate = !date ? "undefined": new Date(date).toLocaleDateString('en-GB', options);
+    return newDate;
+}
+
+const checkName=(name)=>{
+     let nameRegex=RegExp('^[A-Z]{1}[a-zA-Z]{2,}$')
+    if(!nameRegex.test(name))
+        throw "Name Is Incorrect!"
+}
+const Phonevalidation=(name)=>{
+    let nameRegex=RegExp('^[0-9]{10}$')
+   if(!nameRegex.test(name))
+       throw "Phonenumber Is Incorrect!"
+}
+const checkStartDate=(startDate)=>{
+    let currentDate = new Date();
+    if (startDate > currentDate)
+        throw "Start Date is a future date";
+    const diff = Math.abs(currentDate.getTime() - startDate.getTime());
+    if(diff / (1000*60*60*24) > 30){
+        throw "Start Date is a beyond 30 days";
+    }
+}
